7b5e3f36547cc597e7e83fab6730117cef66a362
Rocio Marquesin <marquesinro@gmail.com>
2021-06-11 16:05:04 -0300

    maquetacion header y filtros

--- src/Componentes/FiltrosDeProductos.js
+++ src/Componentes/FiltrosDeProductos.js
@@ -1,15 +1,22 @@
+import "./componentes.css";
 import React from "react";
 import { ReactComponent as ArrowRight } from "./images/arrow-right.svg";
 
 export default function FiltrosDeProductos() {
   return (
-    <div>
-      <div>cantidad de productos visibles 16/32</div>
-      <div>
-        Sort by:
-        <button>Most recent</button>
-        <button>Lowest price</button>
-        <button>Highest price</button>
+    <div className="conteinerFiltrosYCantidad">
+      <div className="conteinerCantidad">
+        <p className="cantidadProductDisponible">16 of 32 products</p>
+      </div>
+      <div className="conteinerSort">
+        <p className="sort">Sort by:</p>
+      </div>
+      <div className="conteinerFiltros">
+        <div className="fitros">
+          <button className="most">Most recent</button>
+          <button className="lowest">Lowest price</button>
+          <button className="highest">Highest price</button>
+        </div>
       </div>
       <ArrowRight />
     </div>
